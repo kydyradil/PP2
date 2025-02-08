f3755f6fad227fa07642b9a3137a2d80ca5d0c8c
kydyradil <a_kydyr@kbtu.kz>
2025-02-08 12:50:51 +0500

    Create 13task.py

---
+++ Lab3/13task.py
@@ -0,0 +1,30 @@
+import random
+
+def guess_the_number():
+    print("Hello! What is your name?")
+    name = input()
+
+    print(f"\nWell, {name}, I am thinking of a number between 1 and 20.")
+
+    secret_number = random.randint(1, 20)
+    attempts = 0
+
+    while True:
+        print("Take a guess.")
+        try:
+            guess = int(input())
+        except ValueError:
+            print("Please enter a valid number.")
+            continue
+
+        attempts += 1
+
+        if guess < secret_number:
+            print("Your guess is too low.")
+        elif guess > secret_number:
+            print("Your guess is too high.")
+        else:
+            print(f"Good job, {name}! You guessed my number in {attempts} guesses!")
+            break
+
+guess_the_number()
